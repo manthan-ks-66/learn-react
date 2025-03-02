d123e626469f845b3487dce2774ba8c61d11d422
Manthan Srivastava <137641968+manthan-ks-66@users.noreply.github.com>
2025-03-02 16:40:02 +0000

    docs: update comment to clarify naming convention for components

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,6 +1,6 @@
 /* the functions you are exporting they are nothing but components only
 
- * and remember that you need to follow the pascal case naming standard
+ * and remember that you need to follow the capitalized naming standard
 
  * capitalized names for functions and file */
 function App() {
